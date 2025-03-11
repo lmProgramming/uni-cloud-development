d4da3b68ddc61a557316e37c7f8eec7d11ec4456
Mikolaj Kubs <mikikubs@gmail.com>
2025-03-11 12:21:08 +0100

    feat: basic sites with routing

--- lab01/web-app/server.ts
+++ lab01/web-app/server.ts
@@ -1,22 +1,23 @@
-import express, { Request, Response } from 'express';
-import path from 'path';
-import morgan from 'morgan';
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import morgan from "morgan";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set EJS as the view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Main route
-app.get('/', (req: Request, res: Response) => {
-  res.render('index', { title: 'Strona główna', message: 'Witaj świecie!' });
+app.get("/", (req: Request, res: Response) => {
+  res.render("index", { title: "Strona główna", message: "Witaj świecie!" });
 });
 
 // Start server
@@ -24,14 +25,35 @@ app.listen(port, () => {
   console.log(`Aplikacja nasłuchuje na porcie ${port}`);
 });
 
-app.post('/', (req: Request, res: Response) => {
-  res.send('Got a POST request');
+app.post("/", (req: Request, res: Response) => {
+  res.send("Got a POST request");
 });
 
-app.put('/user', (req: Request, res: Response) => {
-  res.send('Got a PUT request at /user');
+app.put("/user", (req: Request, res: Response) => {
+  res.send("Got a PUT request at /user");
 });
 
-app.delete('/user', (req: Request, res: Response) => {
-  res.send('Got a DELETE request at /user');
+app.delete("/user", (req: Request, res: Response) => {
+  res.send("Got a DELETE request at /user");
+});
+
+app.get("/contact", (req: Request, res: Response) => {
+  res.render("contact", { title: "Kontakt" });
+});
+
+app.post("/submit-form", (req, res) => {
+  console.log(req.body);
+  res.send("Formularz został wysłany!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).render("404", { title: "Nie znaleziono" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).render("error", { title: "Błąd serwera" });
 });
