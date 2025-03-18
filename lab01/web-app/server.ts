e68606fc54308bf33a8b5c48bd9b9dea721bf77f
Mikolaj Kubs <mikikubs@gmail.com>
2025-03-18 12:43:26 +0100

    feat: footers finally working correctly

--- lab01/web-app/server.ts
+++ lab01/web-app/server.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
